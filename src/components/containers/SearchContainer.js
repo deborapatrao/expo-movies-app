f11231a98a9409902b8e45434363c6d7b6f91a46
deborapatrao <deborapcorreia@gmail.com>
2023-02-01 14:05:58 -0800

    Search functionality

--- src/components/containers/SearchContainer.js
+++ src/components/containers/SearchContainer.js
@@ -1,16 +1,37 @@
 import { Center, Container } from "native-base"
 import { useState } from "react"
 import Form from "../forms/Form"
+import { getTitles } from "../services/api"
 
 const SearchContainer = () => {
     const [isLoading, setIsLoading] = useState(false)
-    const [movies, setMovies] = useState([])
-    const [searchTitle, setSearchTitle] = useState(null)
+    const [titles, setTitles] = useState([])
+    const [searchKeyword, setSearchKeyword] = useState(null)
+
+    const fetchTitles = () => {
+        setIsLoading(true)
+
+        getTitles(searchKeyword, 'movie').then(
+            titles => {
+                setTitles(titles)
+                setIsLoading(false)
+            },
+            error => {
+                console.log('Search container error', error)
+            }
+        )
+    }
+
+    const handleInputChange = searchKeyword => {
+        setSearchKeyword(searchKeyword)
+    }
+
+    console.log('search', searchKeyword)
 
     return(
-        <Container>
+        <Container maxWidth='100%'>
             <Center px={4}>
-                <Form />
+                <Form onInputChange={handleInputChange} fetchTitles={fetchTitles}/>
             </Center>
         </Container>
     )
